90e24311019608dffa28f7c390bf0dd17cfcf5a1
Rameen Fatima <130553793+rameenf1024@users.noreply.github.com>
2024-07-31 20:11:29 +0500

    Javascript code that contains all data

---
+++ music_data.js
@@ -0,0 +1,38 @@
+let songs = [
+{
+    name: 'Jhol',
+    path:'songs/jhol.mpeg',
+    artist:'Maanu,Annural Khalid',
+    cover:'images/jhol.jpg'
+},
+
+{
+    name: 'Promises',
+    path:'songs/Promises.mpeg',
+    artist:'Shamoon Ismail,Annural Khalid',
+    cover:'images/promises.jpg'
+},
+
+{
+    name: 'Nasamajh',
+    path:'songs/Nasamjh.mpeg',
+    artist:'Aditiya Rikhari',
+    cover:'images/nasamajh.jpg'
+},
+
+{
+    name: 'Diet Mountain Dew',
+    path:'songs/mountain-dew.mpeg',
+    artist:'Lana Del Rey',
+    cover:'images/mountain.jpg'
+},
+
+{
+    name: 'Please Please Plaese',
+    path:'songs/please.mpeg',
+    artist:'Sabrina Carpenter',
+    cover:'images/please.jpg'
+},
+
+]
+
